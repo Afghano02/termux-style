deede42e487a741bc54ecb3513408ac489a8e634
Afghano02 <121796344+Afghano02@users.noreply.github.com>
2023-01-29 21:41:09 +0430

    Create termux.py

--- termux.py
+++ termux.py
@@ -5,14 +5,14 @@ command_not_found_handle() {
 PS1='\[\e[31m\]┌─[\[\e[37m\]\T\[\e[31m\]]─────\e[1;93m[root]\e[0;31m───[\#]\n|\n\e[0;31m└─[\[\e[31m\]\e[0;35m\W\[\e[31m\]]────►\e[1;92m'
 
 clear
-echo -e "Created By \e[5mAFGHANO"
+echo -e "Created By \e[5mAfghan-Hacher"
 echo "----------------------------" | lolcat
 echo -e "\e[1mFacebook :@ Afghan Hacher"
 echo -e "\e[1mGithub: github.com/Afghano02/"
 echo "----------------------------" | lolcat
 echo "
 +-+-+-+-+-+-+-+
-|W|e|l|c|o|m|e|   2023
+|W|e|l|c|o|m|e|
 +-+-+-+-+-+-+-+ " | lolcat
 date | lolcat
 echo ""
